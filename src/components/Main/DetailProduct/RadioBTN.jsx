eb4021c47959894c32d446c2d10d402d70831cb6
mhkhatamipanah <mhkhatamipanah@gmail.com>
2024-12-24 11:32:11 +0330

    fixed feature

--- src/components/Main/DetailProduct/RadioBTN.jsx
+++ src/components/Main/DetailProduct/RadioBTN.jsx
@@ -22,7 +22,7 @@ const RadioBTN = ({ title, data, onChange, reset }) => {
       <div className="flex gap-2 gap-y-4 items-center my-3 flex-wrap">
         {data.map((e, i) => {
           return (
-            <div key={i}>
+            <div className="flex items-center" key={i}>
               <input
                 type="radio"
                 name={`radioGroup-${title}`} // اضافه کردن عنوان برای جلوگیری از تداخل گروه‌ها
@@ -33,12 +33,12 @@ const RadioBTN = ({ title, data, onChange, reset }) => {
               />
 
               <label
-                className={``}
+                className={`flex items-center p-1 bg-[#0d948928] border-gray-200 rounded-xl border-2`}
                 htmlFor={`${e.value}${i}`}
               >
-                {e?.productCode && <span className=" rounded-lg "> {`${e.productCode} :`} </span>}
+                {e?.productCode && <span className="px-1 rounded-lg "> {`${e.productCode} :`} </span>}
 
-                <span className={`px-2 py-1 bg-gray-200 text-sm sm:text-md rounded-lg cursor-pointer hover:bg-green-700 hover:text-white  transition-all duration-150 ${radioValue === e.value ? "text-white bg-green-600 !border-none" : ""}`}>
+                <span className={`px-1.5 py-1.5 bg-gray-50 text-sm sm:text-md rounded-lg cursor-pointer hover:bg-green-600 hover:text-white  transition-all duration-150 ${radioValue === e.value ? "text-white bg-green-700 !border-none" : ""}`}>
                   {e.value}
                 </span>
 
